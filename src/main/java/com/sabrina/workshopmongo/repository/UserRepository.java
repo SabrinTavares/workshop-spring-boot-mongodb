121609b547ee8588d6b9dbbe8130f6f9064e5faf
Sabrina Tavares <sabrinacries@gmail.com>
2024-10-03 00:17:18 -0300

    Conectando MongoDb com repository e service

---
+++ src/main/java/com/sabrina/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sabrina.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sabrina.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+}
